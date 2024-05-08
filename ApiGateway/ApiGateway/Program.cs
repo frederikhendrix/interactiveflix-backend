fa28e8d832a6b9146e6a95e50fddd6830f303c7f
frederikhendrix <rikhendrix@live.nl>
2024-05-08 17:07:58 +0200

    ocelot implemented for blobstorageservice and videometadataservice

--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -3,7 +3,7 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("configuration.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
