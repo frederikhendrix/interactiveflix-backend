a63449257604ac15e9a88dd78cbaeba4b7f4ca00
frederikhendrix <rikhendrix@live.nl>
2024-05-12 18:48:26 +0200

    changed the docker compose and added JWT authentication with Firebase

--- ApiGateway/ApiGateway/Program.cs
+++ ApiGateway/ApiGateway/Program.cs
@@ -1,12 +1,41 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure JWT Bearer Authentication to use Firebase's parameters
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer("Firebase", options =>
+{
+    options.Authority = "https://securetoken.google.com/interactiveflix";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = "https://securetoken.google.com/interactiveflix",
+        ValidateAudience = true,
+        ValidAudience = "interactiveflix",
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
+
+// Configure the app to use middleware
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseOcelot().Wait();
 
